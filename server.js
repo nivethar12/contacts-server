a7f41bc65a36db8de8f94b985e099a1716f42c81
nivethar12 <nivetha.r12@gmail.com>
2019-04-15 18:18:57 +0530

    Fixed add or edit feature

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const urlencodedParser = bodyParser.urlencoded({
   extended: false,
 });
 
+// Cors Header
 app.use(function(req, res, next) {
   const origin = req.headers.origin;
   res.header('Access-Control-Allow-Origin', origin);
@@ -17,37 +18,81 @@ app.use(function(req, res, next) {
   next();
 });
 app.use(bodyParser.json());
+
+// Cors Header
 app.get('/', (req, res) => {
   res.send('nivetha is great!');
 });
+
+const CONTACTS_FILE = './contacts.json';
+
 app.get('/contacts', (req, res) => {
-  let content = fs.readFileSync('./contacts.json');
+  let content = fs.readFileSync(CONTACTS_FILE);
   let data = content.toString();
   res.json(JSON.parse(data));
 });
+
 app.get('/contactsDetails', (req, res) => {
   const id = req.query.id;
-  console.log('iddddddddd', id);
-  const content = fs.readFileSync('./contacts.json');
-  const data = content.toString();
-  const obj = JSON.parse(data);
-  console.log(data);
-  const contact = obj.find(item => item.id === parseInt(id));
-  console.log(contact);
+  const fileContent = fs.readFileSync(CONTACTS_FILE).toString();
+  const contacts = JSON.parse(fileContent);
+  const contact = contacts.find(item => item.id === parseInt(id));
   res.json(contact);
 });
 
-app.post('addorEdit', urlencodedParser, function(req, res) {
-  const name = req.body.contactName;
-  console.log('contact', name);
-  const successdata = {
-    status: true,
-    message: 'Record created - success',
-  };
-  const failuredata = {
-    status: false,
-    message: 'Error creating new account',
-  };
+app.post('/addorEdit', urlencodedParser, function(req, res) {
+  try {
+    const fileContent = fs.readFileSync(CONTACTS_FILE).toString();
+    let contacts = JSON.parse(fileContent);
+    const payload = req.body;
+    if (payload.id) {
+      contacts = contacts.map(contact => {
+        if (contact.id == payload.id) {
+          contact.contactName = payload.contactName;
+          contact.mobile = payload.mobile;
+          contact.email = payload.email;
+        }
+        return contact;
+      });
+    } else {
+      contacts.push({
+        contactImage: 'assets/images/man.png',
+        contactName: payload.contactName,
+        mobile: payload.mobile,
+        email: payload.email,
+        favourite: false,
+        id: contacts.length + 1,
+      });
+    }
+    console.log(contacts);
+    fs.writeFileSync(CONTACTS_FILE, JSON.stringify(contacts, null, 2));
+    res.json({
+      status: true,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      status: false,
+    });
+  }
+});
+
+app.post('/addfavorate', urlencodedParser, function(req, res) {
+  const id = req.body.id;
+  // Delete Code
   res.json(successdata);
 });
+
+app.post('/removefavorate', urlencodedParser, function(req, res) {
+  const name = req.body.contactName;
+  // Delete Code
+  res.json(successdata);
+});
+
+app.post('/delete', urlencodedParser, function(req, res) {
+  const name = req.body.contactName;
+  // Delete Code
+  res.json(successdata);
+});
+
 app.listen(3000, () => console.log('app listening on port 3000!'));
